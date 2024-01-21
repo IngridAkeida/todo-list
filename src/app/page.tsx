ff00ee5e8c9c1b0805a5a80785002f175cc5312a
IngridAkeida <ingridsanchesp@gmail.com>
2024-01-21 15:20:14 +0100

    add - type folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 "use client"
 
+import { useState } from "react";
+
 const Page = () =>{
+
+  const [ list, setList] = useState([]);
+
+  
 
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center text-2xl bg-gradient-to-tl from-purple-600 via-blue-400 to-blue-600">
