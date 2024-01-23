771134511255185790b3ea978ea7efd6ab69c0eb
IngridAkeida <ingridsanchesp@gmail.com>
2024-01-23 17:11:17 +0100

    create - reducer folder

--- src/reducers/listReducers.ts
+++ src/reducers/listReducers.ts
@@ -1,10 +1,46 @@
 import { TodoItem } from "@/types/TodoItem";
 
-listActions = ;
+
+type AddAction = {
+  type: 'add';
+  payload: {
+    label: string;
+  }
+}
+
+type EditTextAction = {
+  type: 'editText';
+  payload: {
+    id: number;
+    newText: string;
+  }
+}
+
+type RemoveItem = {
+  type: 'removeItem';
+  payload: {
+    id: number;
+  }
+}
+
+type ToggleDone = {
+  type: 'toggleDone';
+  payload: {
+    id: number;
+  }
+}
+
+type listActions = AddAction | EditTextAction | RemoveItem | ToggleDone;
 
 export const listReducer = (list:TodoItem[], action: listActions) => {
-  //executar aguma acao
+  //executar alguma acao
   //retornar a lista atualizada
-
-  return list;
+  switch(action.type) {
+    case 'add':
+    case 'editText':
+    case 'removeItem':
+    case 'toggleDone':
+    default:
+      return list;
+  } 
 }
